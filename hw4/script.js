6e73f3f7f765c863b45868fc1d337481bb2fbc47
neilbantoc <neilbantoc@gmail.com>
2017-03-06 03:50:18 -0500

    Initial tree

--- hw4/script.js
+++ hw4/script.js
@@ -366,10 +366,115 @@ function isGame(objectClicked) {
  */
 function createTree(treeData) {
 
-    // ******* TODO: PART VI *******
+  // var group = d3.select("#tree");
+  // var svgBounds = group.node().getBoundingClientRect();
+  // var width = svgBounds[0];
+  // var height = svgBounds[1];
+  //
+  // var tree = d3.tree()
+  //   .size(svgBounds);
+  //
+  // var treeData = d3.stratify()
+  //   .id(function(d) {
+  //     var id = d.id;
+  //     id = id.replace(d["Opponent"], "");
+  //     id = id.replace(d["Team"], "");
+  //     return id;
+  //   })
+  //   .parentId(function(d) {
+  //     return d.ParentGame;
+  //   })
+  //   (treeData);
+  //
+  // root = d3.hierarchy(treeData, function(d) { return d.children; });
+  // root.x0 = height / 2;
+  // root.y0 = 0;
+  //
+  // var treeData = tree(root);
+  //
+  // // Compute the new tree layout.
+  // var nodes = treeData.descendants();
+  // var links = treeData.descendants().slice(1);
+  //
+  // nodes.forEach(function(d){ d.y = d.depth * 180});
+  //
+  // var node = d3.selectAll('g.node')
+  //     .data(nodes, function(d) {return d.id});
+  //
+  // var nodeEnter = node.enter().append('g')
+  //     .attr('class', 'node')
+  //     .attr("transform", function(d) {
+  //       return "translate(" + source.y0 + "," + source.x0 + ")";
+  //   })
 
+  var g = d3.select("#tree").attr("transform", "translate(100, 0)");
+  var width = 300;
+  var height = 900;
+  var padding = 10;
 
-};
+  var treeData = d3.stratify()
+    .id(function(d) {
+      var id = d.id;
+      id = id.replace(d["Opponent"], "");
+      id = id.replace(d["Team"], "");
+      return id;
+    })
+    .parentId(function(d) {
+      return d.ParentGame;
+    })
+    (treeData);
+
+  // declares a tree layout and assigns the size
+  var treemap = d3.tree()
+      .size([height-padding, width-padding]);
+
+  //  assigns the data to a hierarchy using parent-child relationships
+  var nodes = d3.hierarchy(treeData, function(d) {
+    return d.children;
+  });
+
+  // maps the node data to the tree layout
+  nodes = treemap(nodes);
+
+  var link = g.selectAll(".link")
+    .data(nodes.descendants().slice(1))
+    .enter().append("path")
+    .attr("class", "link")
+    .style("stroke", function(d) { return d.data.level; })
+    .attr("d", function(d) {
+       return "M" + d.y + "," + d.x
+         + "C" + (d.y + d.parent.y) / 2 + "," + d.x
+         + " " + (d.y + d.parent.y) / 2 + "," + d.parent.x
+         + " " + d.parent.y + "," + d.parent.x;
+       });
+
+   // adds each node as a group
+   var node = g.selectAll(".node")
+      .data(nodes.descendants())
+      .enter()
+      .append("g")
+      .classed("node", true)
+      .classed("winner", function(d) {
+        return d["data"]["data"]["Wins"] > 0;
+      })
+      .attr("transform", function(d) {
+        return "translate(" + d.y + "," + d.x + ")";
+      });
+
+  // adds symbols as nodes
+  node.append("circle")
+      .attr("r", 5);
+
+  node.append("text")
+  .attr("dy", ".35em")
+  .attr("x", function(d) {
+    return d.children ? -5 : 5;
+  })
+  .style("text-anchor", function(d) {
+    return d.children ? "end" : "start"; })
+  .text(function(d) {
+    return d["data"]["data"]["Team"]; });
+  };
 
 /**
  * Updates the highlighting in the tree based on the selected team.
